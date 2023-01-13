20fc75dac8c4ed75182d6ac57e90f2c86fdec19a
achchu <vpachchu27@gmail.com>
2023-01-13 21:38:07 +0530

    First Button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="primary">Click me!</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
